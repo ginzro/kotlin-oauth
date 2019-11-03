9b22a71a25d5e015837cce5c589a137505062841
Ginjiro Ueta <ginjiro.mmj@gmail.com>
2019-11-03 09:07:39 +0800

    tokenを取得する処理を実装

--- src/Application.kt
+++ src/Application.kt
@@ -1,5 +1,6 @@
 package com.ginzro
 
+import com.google.gson.Gson
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
@@ -7,18 +8,24 @@ import io.ktor.http.*
 import io.ktor.html.*
 import kotlinx.html.*
 import kotlinx.css.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.apache.commons.codec.binary.Base64
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import kotlin.random.Random
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 object ConstValue {
-    const val CLIENT_ID = "oauth-client-1"
-    const val CLIENT_SECRET = "oauth-client-secret-1"
-    val REDIRECT_URI = "http://localhost:9000/callback"
+  const val CLIENT_ID = "oauth-client-1"
+  const val CLIENT_SECRET = "oauth-client-secret-1"
+  val REDIRECT_URI = "http://localhost:9000/callback"
 }
 
 object DB {
-    val states: MutableMap<String, String> = mutableMapOf()
+  val states: MutableMap<String, String> = mutableMapOf()
 }
 
 
@@ -26,99 +33,98 @@ object DB {
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
 
-    routing {
-        get("/") {
-            call.respondHtml {
-                body {
-                    h1 { +"OAuth徹底入門 with Kotlin" }
-                    a(href = "http://localhost:9000/authorize") { +"AUTHORIZE" }
-                }
-            }
+  routing {
+    get("/") {
+      call.respondHtml {
+        body {
+          h1 { +"OAuth徹底入門 with Kotlin" }
+          a(href = "http://localhost:9000/authorize") { +"AUTHORIZE" }
         }
-
-        get("/authorize") {
-            call.respondRedirect(false) {
-                host = "localhost"
-                port = 9001
-                path("authorize")
-
-                parameters["response_type"] = "code"
-                parameters["client_id"] = ConstValue.CLIENT_ID
-                parameters["redirect_uri"] = ConstValue.REDIRECT_URI
-
-                val state = getRandomString()
-                DB.states[ConstValue.CLIENT_ID] = state
-                parameters["state"] = state
-            }
-        }
-
-        get("/callback") {
-            val code = call.request.queryParameters["code"]
-            val state = call.request.queryParameters["state"]
-
-            call.respondHtml {
-                body {
-                    h1 { +"OAuth徹底入門 with Kotlin" }
-                    h2 { +"response" }
-                    ul {
-                        li { +"code is $code" }
-                        li { +"pre state is ${DB.states[ConstValue.CLIENT_ID]}" }
-                        li { +"state is $state" }
-                    }
-                    h2 { +"next" }
-                    a(href = "http://localhost:9001/token") { +"TOKEN" }
-                }
-            }
-        }
-
-
-        get("/html-dsl") {
-
-            val name = call.request.queryParameters["name"]
-            if (name != null) {
-                call.respondText("$name HELLO WORLD!", contentType = ContentType.Text.Plain)
-            } else {
-                call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-            }
-
-        }
-
-        get("/styles.css") {
-            call.respondCss {
-                body {
-                    backgroundColor = Color.red
-                }
-                p {
-                    fontSize = 2.em
-                }
-                rule("p.myclass") {
-                    color = Color.blue
-                }
-            }
-        }
+      }
     }
+
+    get("/authorize") {
+      call.respondRedirect(false) {
+        host = "localhost"
+        port = 9001
+        path("authorize")
+
+        parameters["response_type"] = "code"
+        parameters["client_id"] = ConstValue.CLIENT_ID
+        parameters["redirect_uri"] = ConstValue.REDIRECT_URI
+
+        val state = getRandomString()
+        DB.states[ConstValue.CLIENT_ID] = state
+        parameters["state"] = state
+      }
+    }
+
+    get("/callback") {
+      val code = call.request.queryParameters["code"]
+      val state = call.request.queryParameters["state"]
+
+      if (code == null || state == null || state != DB.states[ConstValue.CLIENT_ID]) {
+        throw Exception("invalid authorization code")
+      }
+
+      val req = Request.Builder().apply {
+        val utf8 = StandardCharsets.UTF_8.toString()
+        val clientIdByteArray = URLEncoder.encode(ConstValue.CLIENT_ID, utf8)
+        val secretByteArray = URLEncoder.encode(ConstValue.CLIENT_SECRET, utf8)
+        addHeader("Content-Type", "application/x-www-form-urlencoded")
+        val idAndPass = Base64.encodeBase64String("$clientIdByteArray:$secretByteArray".toByteArray())
+        addHeader("Authorization", "Basic $idAndPass")
+        val body = "grant_type=authorization_code&code=$code&redirect_uri${ConstValue.REDIRECT_URI}".toRequestBody()
+        method("POST", body)
+        url("http://localhost:9001/token")
+      }.build()
+      val res = OkHttpClient().newCall(req).execute()
+
+
+      val responseBody = Gson().fromJson(res.body!!.string(), TokenResp::class.java)!!
+      println(responseBody)
+      call.respondHtml {
+        body {
+          h1 { +"OAuth徹底入門 with Kotlin" }
+          h2 { +"response" }
+          ul {
+            li { +"code is $code" }
+            li { +"pre state is ${DB.states[ConstValue.CLIENT_ID]}" }
+            li { +"state is $state" }
+            li { +"token is ${responseBody.access_token}" }
+            li { +"token type is ${responseBody.token_type}" }
+          }
+        }
+      }
+    }
+  }
 }
 
 fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
-    style(type = ContentType.Text.CSS.toString()) {
-        +CSSBuilder().apply(builder).toString()
-    }
+  style(type = ContentType.Text.CSS.toString()) {
+    +CSSBuilder().apply(builder).toString()
+  }
 }
 
 fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
-    this.style = CSSBuilder().apply(builder).toString().trim()
+  this.style = CSSBuilder().apply(builder).toString().trim()
 }
 
 suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
-    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+  this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
 }
 
 fun getRandomString(): String {
-    val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-    return (1..20)
-        .map { Random.nextInt(0, charPool.size) }
-        .map { charPool[it] }
-        .joinToString("")
+  val charPool = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+  return (1..20)
+    .map { Random.nextInt(0, charPool.size) }
+    .map { charPool[it] }
+    .joinToString("")
 }
 
 
+data class TokenResp(
+  val access_token: String,
+  val token_type: String,
+  val scope: String
+)
